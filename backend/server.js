fade36a4d498156047d894c51704b0b271368595
Prem <56423583+Prem1803@users.noreply.github.com>
2021-05-22 20:04:34 +0530

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -27,10 +27,15 @@ const _dirname = path.resolve();
 app.use("/uploads", express.static(path.join(_dirname, "/uploads")));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname,"/../client/build")));
 
+app.use("/api/users", userRoutes);
+app.use("/api/announcements", announcementRoutes);
+app.use("/api/blogs", blogRoutes);
+app.use("/api/projects", projectRoutes);
+app.use("/api/upload", uploadRoutes);
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "/../client/build/index.html"));
   });
 }
 
